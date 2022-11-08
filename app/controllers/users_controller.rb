c5a168aacae7993946bfceb41794044584cd57f2
Nikos Kalogeropoulos <nikoskalogeropoulos12@gmail.com>
2022-11-08 21:24:16 +0200

    Create user table, model, controller and implement basic password hash functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+    def show
+
+    end
+
+    def new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            redirect_to new_user_url
+        else 
+            flash[:errors] = user.errors.full_messages
+            redirect_to new_user_url
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # add show for link of the user
+  resources :users, only: [:create, :new, :show]
+end
