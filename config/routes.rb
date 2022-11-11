0f0b90a69be2f208094d4fca2b0db6851718cbbc
Nikos Kalogeropoulos <nikoskalogeropoulos12@gmail.com>
2022-11-11 10:57:01 +0200

    Add a root route on movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: "movies#index"
   # add show for link of the user
   resources :users, only: [:create, :new]  do
      resources :movies, only: [:index]
